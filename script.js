0c1c1a0fde60f5f2cbca6bdf4a8af6b4aca44834
Privatech38 <ziga.slatnar.stagar@gmail.com>
2023-02-03 16:56:18 +0100

    Replaced script js with ts

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let genM;
 let genF;
 
+const chances = new Map();
+
 function generateChancesAndGraph() {
     genM = document.getElementById("genM").value;
     genF = document.getElementById("genF").value;
     let form = document.getElementById("genForm");
-    document.getElementById("results").innerHTML = genM + genF;
+    // Create arrays
+    const genMArray = [];
+    const genFArray = [];
+    for(let i = 0; i < 8; i++) {
+        genMArray.push(genM.substring(i, (i+1)));
+        genFArray.push(genF.substring(i, (i+1)));
+    }
+    document.getElementById("results").innerHTML = genMArray.pop(", ") + "; " + genFArray.pop(", ");
+}
+
+function calculateChances() {
+    for(let i = 0; i < 4; i++) {
+
+    }
 }
