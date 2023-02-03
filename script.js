4d25cad649c0b74326953cdd6ba0e4d5cbb6fe01
Privatech38 <ziga.slatnar.stagar@gmail.com>
2023-02-03 19:07:20 +0100

    Made auto create for gens with ts

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-let genM;
-let genF;
-
-const chances = new Map();
-
-function generateChancesAndGraph() {
-    genM = document.getElementById("genM").value;
-    genF = document.getElementById("genF").value;
-    let form = document.getElementById("genForm");
-    // Create arrays
-    const genMArray = [];
-    const genFArray = [];
-    for(let i = 0; i < 8; i++) {
-        genMArray.push(genM.substring(i, (i+1)));
-        genFArray.push(genF.substring(i, (i+1)));
+var genM;
+var genF;
+var gens = [
+    ["A", "At", "a"],
+    ["B", "b"],
+    ["C", "chd", "chl", "ch", "c"],
+    ["D", "d"],
+    ["E", "Es", "ej", "e"],
+    ["V", "v"],
+    ["W", "w"]
+];
+function loadPage() {
+    var box = document.getElementById("forms");
+    if (box != null) {
+        // MALE
+        var divElementMale = document.createElement("div");
+        divElementMale.setAttribute("id", "maleGens");
+        // Add text
+        var male = document.createElement("label");
+        male.appendChild(document.createTextNode("MALE"));
+        divElementMale.appendChild(male);
+        divElementMale.appendChild(document.createElement("br"));
+        // Generate selects
+        generateSelects(divElementMale);
+        // FEMALE
+        var divElementFemale = document.createElement("div");
+        divElementFemale.setAttribute("id", "femaleGens");
+        // Add text
+        var female = document.createElement("label");
+        female.appendChild(document.createTextNode("FEMALE"));
+        divElementFemale.appendChild(female);
+        divElementFemale.appendChild(document.createElement("br"));
+        // Generate selects
+        generateSelects(divElementFemale);
+        // Append to main div
+        box.appendChild(divElementFemale);
+        box.appendChild(divElementMale);
     }
-    document.getElementById("results").innerHTML = genMArray.pop(", ") + "; " + genFArray.pop(", ");
+    else {
+        console.log("box is null");
+    }
 }
-
-function calculateChances() {
-    for(let i = 0; i < 4; i++) {
-
-    }
+function generateSelects(divElement) {
+    gens.forEach(function (group) {
+        var selectObject = document.createElement("select");
+        group.forEach(function (firstGen) {
+            group.forEach(function (secondGen) {
+                var genCombo = firstGen + " " + secondGen;
+                var optionObject = document.createElement("option");
+                optionObject.setAttribute("value", genCombo);
+                optionObject.appendChild(document.createTextNode(genCombo));
+                selectObject.appendChild(optionObject);
+            });
+        });
+        divElement.appendChild(selectObject);
+        divElement.appendChild(document.createElement("br"));
+    });
+}
+function generate() {
 }
--- script.ts
+++ script.ts
@@ -8,10 +8,56 @@ const gens: string[][] = [
     ["D", "d"],
     ["E", "Es", "ej", "e"],
     ["V", "v"],
-    ["W", "w"]];
+    ["W", "w"]
+];
 
-function generateSelects() {
+function loadPage() {
+    let box = document.getElementById("forms");
+    if (box != null) {
+        // MALE
+        let divElementMale: HTMLDivElement = document.createElement("div");
+        divElementMale.setAttribute("id", "maleGens");
+        // Add text
+        let male: HTMLLabelElement = document.createElement("label");
+        male.appendChild(document.createTextNode("MALE"));
+        divElementMale.appendChild(male);
+        divElementMale.appendChild(document.createElement("br"));
+        // Generate selects
+        generateSelects(divElementMale);
+        // FEMALE
+        let divElementFemale: HTMLDivElement = document.createElement("div");
+        divElementFemale.setAttribute("id", "femaleGens");
+        // Add text
+        let female: HTMLLabelElement = document.createElement("label");
+        female.appendChild(document.createTextNode("FEMALE"));
+        divElementFemale.appendChild(female);
+        divElementFemale.appendChild(document.createElement("br"));
+        // Generate selects
+        generateSelects(divElementFemale);
+        // Append to main div
+        box.appendChild(divElementFemale);
+        box.appendChild(divElementMale);
+    } else {
+        console.log("box is null");
+    }
     
+}
+
+function generateSelects(divElement: HTMLDivElement): void {
+    gens.forEach(group => {
+        let selectObject = document.createElement("select");
+        group.forEach(firstGen => {
+            group.forEach(secondGen => {
+                let genCombo: string = firstGen + " " + secondGen;
+                let optionObject: HTMLOptionElement = document.createElement("option");
+                optionObject.setAttribute("value", genCombo);
+                optionObject.appendChild(document.createTextNode(genCombo));
+                selectObject.appendChild(optionObject);
+            });
+        });
+        divElement.appendChild(selectObject);
+        divElement.appendChild(document.createElement("br"));
+    });
 }
 
 function generate(): void {
